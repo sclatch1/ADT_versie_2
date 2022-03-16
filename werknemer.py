c8b15319eab6d669ad2b26f6d19f1c08a09e85f1
sclatch1 <52760409+sclatch1@users.noreply.github.com>
2022-03-16 11:05:36 +0100

    Add files via upload

---
+++ werknemer.py
@@ -0,0 +1,36 @@
+#thomas th maakt, Daan test
+## functionaliteit
+class werknemer():
+    def add_werknemer(voornaam, achternaam, workload):
+        """
+        voeg een werknemer toe aan de ADT indien deze nog niet bestaat
+
+        preconditie : voornaam en achternaam zijn geldige namen, workload is positief integer
+        postconditie : aantal_personeel gaat met 1 omhoog
+
+        : param voornaam : een string die de voornaam van de werknemer
+        voorstelt :
+        : param achternaam : een string die de achternaam van de werknemer
+        voorstelt :
+        : param workload : een integer die de workload van de werknemer in credits
+        voorstelt :
+        : return : geeft niets terug
+        """
+
+        pass
+
+    def verwijder_werknemer(voornaam, achternaam):
+        """
+        verwijder een werknemer van de ADT indien deze bestaat
+
+        preconditie : voornaam en achternaam staan in de ADT
+        postconditie : aantal_personeel gaat met 1 omlaag
+
+        : param voornaam : een string die de voornaam van de werknemer
+        voorstelt :
+        : param achternaam : een string die de achternaam van de werknemer
+        voorstelt :
+        : return : geeft niets terug
+        """
+
+        pass
